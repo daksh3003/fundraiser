16052f06d87f71a32bfd85e11b6451f78f8e7f0a
daksh3003 <dakshpandey3003@gmail.com>
2024-10-01 23:52:10 +0530

    contract deployed and constants created

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,14 @@
 // scripts/deploy.js
 
 const hre = require("hardhat");
-
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
 async function main() {
   // Compile the contracts
 
 
   // Get the contract to deploy
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding"); // Replace with your contract name
-  const crowdFunding = await Token.deploy(); // You can pass constructor arguments here if needed
+  const crowdFunding = await CrowdFunding.deploy(); // You can pass constructor arguments here if needed
 
   await crowdFunding.deployed();
 
